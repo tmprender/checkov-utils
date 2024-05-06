01ae12a457c81f95356a6a6101851dd4dc9e6413
Tom Prenderville <tmprender@gmail.com>
2024-05-06 10:36:54 -0400

    cleaned up, working pre-plan, fixed scope for working dir

--- server/app.py
+++ server/app.py
@@ -66,53 +66,30 @@ def tf_run_task_review():
     callback_url = data['task_result_callback_url']
     download_url = data.get('configuration_version_download_url')  # tar of code to scan, can be null
     tf_plan_dl_url = data.get('plan_json_api_url')  # json output, available in post-plan only
-    
+    working_dir = data.get('workspace_working_direectory')  # tf working dir if applicable
+
     headers = {
         'Content-Type': 'application/vnd.api+json',
         'Authorization': "Bearer " + access_token
     }
-    #pre_plan
+
+    # checkov scan results
+    scan = None
+
+    # pre_plan
     if data['stage'] == 'pre_plan':
         if download_url:
-            
-            #pre_plan_analysis(download_url, headers)
-
-            # Download and extract tar.gz
-            try:
-                time.sleep(5)  # why is TFC giving me a download URL before it's ready...
-                response = requests.get(download_url, headers=headers)
-                response.raise_for_status()
-                target_dir = './temp_dir'
-                os.makedirs(target_dir, exist_ok=True)
-                decode_and_unpack_tar_gz(response.content, target_dir, is_base64_encoded=False)
-                # Run Checkov on the directory
-                scan = run_checkov(target_dir, cli_flags=None, is_file=False)
-                # parse for just TF checks - quick fix
-                scan = scan[0]
-                # clean up
-                shutil.rmtree(target_dir)
-            except Exception as e:
-                return jsonify({"error": e}), 500
+            scan = pre_plan_analysis(download_url, working_dir, headers)
         else:
             return jsonify({"error": "No configuration version download URL provided for pre-plan."}), 400
     
     # post_plan    
-    elif data['stage'] == 'post_plan':
-        
-        #post_plan_analysis(tf_plan_dl_url, headers)
+    elif data['stage'] == 'post_plan':   
+        if tf_plan_dl_url:   
+            scan = post_plan_analysis(tf_plan_dl_url, headers)
+        else:
+            return jsonify({"error": "No plan_json_api_url provided; expected for post-plan"})
 
-        # Download the plan JSON
-        try:
-            response = requests.get(tf_plan_dl_url, headers=headers)
-            response.raise_for_status()
-            plan_json = response.json()
-            with open("tfplan.json", "w") as f:
-                json.dump(plan_json, f)
-            # Run Checkov on the plan file
-            scan = run_checkov("tfplan.json", None, is_file=True)
-            os.remove("tfplan.json")
-        except Exception as e:
-            return jsonify({"error": e}), 500
     # invalid stage / not supported
     else:
         return jsonify({"error": "Invalid stage."}), 400
@@ -157,12 +134,15 @@ def tf_run_task_review():
         }
     }
 
-    # Return results to tfc/e
+    # return results to TFC/E
     response = requests.patch(callback_url, headers=headers, data=json.dumps(payload))
     response.raise_for_status()
     return jsonify(scan)
 
-# Internals
+
+#############
+# Internals #
+#############
 
 def decode_and_unpack_tar_gz(file_content, target_dir, is_base64_encoded=True):
     # Decode the base64-encoded file if necessary
@@ -189,9 +169,9 @@ def decode_and_unpack_tar_gz(file_content, target_dir, is_base64_encoded=True):
 def run_checkov(target, cli_flags, is_file=False):
     if cli_flags is None:
         cli_flags = []
-    # Determine the Checkov scan target flag based on the input
+    # set file/directory flag
     target_flag = '-f' if is_file else '-d'
-    # Run the Checkov scan
+    # run the Checkov scan
     result = subprocess.run(
         ['checkov', target_flag, target, '--output', 'json'] + cli_flags,
         capture_output=True,
@@ -204,3 +184,45 @@ def run_checkov(target, cli_flags, is_file=False):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+
+def pre_plan_analysis(download_url, working_dir, headers):
+    try:
+        # decode and unpack workspace contents
+        time.sleep(5)  # why is TFC giving me a download URL before it's ready...
+        response = requests.get(download_url, headers=headers)
+        response.raise_for_status()
+        target_dir = './temp_dir'
+        os.makedirs(target_dir, exist_ok=True)
+        decode_and_unpack_tar_gz(response.content, target_dir, is_base64_encoded=False)
+        # check for working directory
+        if working_dir:
+            target_dir += working_dir
+        # Run Checkov on the directory
+        scan = run_checkov(target_dir, cli_flags=None, is_file=False)
+        # parse for just TF checks - quick fix
+        scan = scan[0]
+        # clean up
+        shutil.rmtree(target_dir)
+        # return scan results
+        return scan
+    except Exception as e:
+        return jsonify({"error": e}), 500
+
+
+def post_plan_analysis(tf_plan_dl_url, headers):
+    try:
+        # download plan.json
+        response = requests.get(tf_plan_dl_url, headers=headers)
+        response.raise_for_status()
+        plan_json = response.json()
+        with open("tfplan.json", "w") as f:
+            json.dump(plan_json, f)
+        # run checkov on the plan file
+        scan = run_checkov("tfplan.json", None, is_file=True)
+        # clen up
+        os.remove("tfplan.json")
+        # return scan results
+        return scan
+    except Exception as e:
+        return jsonify({"error": e}), 500
